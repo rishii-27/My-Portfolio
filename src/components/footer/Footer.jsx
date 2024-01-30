6bbc3d997b7a119c790336ef2bd6da54d7543a27
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2024-01-30 16:17:29 +0530

    Fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,37 +1,61 @@
-import React from 'react';
-import { BsLinkedin } from 'react-icons/bs';
-import { FaGithub } from 'react-icons/fa';
-import { FaAngellist } from 'react-icons/fa'
-import './footer.css';
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import "./footer.css";
 
 const Footer = () => {
-  
   let getYear = () => {
     let currentYear = new Date().getFullYear();
     return currentYear;
   };
-  
+
   return (
     <footer>
-      <a href="#home" className="footer__logo">Hrishikesh Joshi</a>
+      <a href="#home" className="footer__logo">
+        Hrishikesh Joshi
+      </a>
       <ul className="permalinks">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Skills</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Skills</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
       <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/meri-gogichashvili/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Meri-MG" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      <a href="https://angel.co/u/meri-gogichashvili" target="_blank" rel="noreferrer" ><FaAngellist /></a>
+        <a
+          href="https://www.linkedin.com/in/rishii27/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/rishii-27" target="_blank" rel="noreferrer">
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/rishii_27/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </a>
       </div>
       <div className="footer__copyright">
         <small>&copy; ET {getYear()}. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
